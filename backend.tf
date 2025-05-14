5629dc56af36779b1635a6afe5073525f71902cc
Nikita Kashyap <161322388+nikitaks97@users.noreply.github.com>
2025-05-14 16:16:06 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "statebucket0"
-    key = "statebucket0/s3-tf"
-    region = "us-east-1"
+    bucket  = "statebucket0"
+    key     = "statebucket0/s3-tf"
+    region  = "us-east-1"
     encrypt = true
   }
 }
